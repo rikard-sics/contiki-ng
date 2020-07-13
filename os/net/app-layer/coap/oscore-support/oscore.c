9dc0ce5f5943109f95316b70cbd37811fa58ca52
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-07-13 14:09:41 +0100

    OSCORE: Allow enabling registering contexts to URIs via OSCORE_EP_CTX_ASSOCIATION

--- os/net/app-layer/coap/oscore-support/oscore.c
+++ os/net/app-layer/coap/oscore-support/oscore.c
@@ -81,7 +81,7 @@ coap_is_request(const coap_message_t *coap_pkt)
   return coap_pkt->code >= COAP_GET && coap_pkt->code <= COAP_DELETE;
 }
 bool
-oscore_protected_request(const coap_message_t *request)
+oscore_is_request_protected(const coap_message_t *request)
 {
   return request != NULL && coap_is_option(request, COAP_OPTION_OSCORE);
 }
@@ -725,8 +725,10 @@ oscore_init(void)
   oscore_crypto_init();
 #endif
 
+#ifdef OSCORE_EP_CTX_ASSOCIATION
   /* Initialize the security_context storage, the token - seq association storrage and the URI - security_context association storage. */
   oscore_ep_ctx_store_init();
+#endif
 }
 
 #ifdef WITH_GROUPCOM
