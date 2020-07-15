7c5e0b812baf1c11a6c3a3d4f7262fdbcc52fc80
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-07-15 08:44:41 +0100

    OSCORE: Remove cbor implementation and use nanocbor

--- os/sys/cc.h
+++ os/sys/cc.h
@@ -172,4 +172,17 @@
 #define CC_CONCAT3(s1, s2, s3) s1##s2##s3
 #define CC_CONCAT_EXT_3(s1, s2, s3) CC_CONCAT3(s1, s2, s3)
 
+/**
+ * A C preprocessing macro to obtain the length of a C array.
+ */
+#define CC_ARRAY_SIZE(a) (sizeof(a)/sizeof(*(a)))
+
+/**
+ * A C preprocessing macro to obtain size of a field in a struct.
+ */
+#define CC_FIELD_SIZEOF(t, f) (sizeof(((t*)0)->f))
+
+#define CC_STRINGIFY_IMPL(s) #s
+#define CC_STRINGIFY(s) CC_STRINGIFY_IMPL(s)
+
 #endif /* CC_H_ */
