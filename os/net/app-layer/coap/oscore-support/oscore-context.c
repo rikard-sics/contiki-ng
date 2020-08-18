f14860b819cb1a62eceaf07561675b1e55d30bc8
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-08-18 14:59:16 +0100

    OSCORE: Refactor the sliding window into its own file

--- os/net/app-layer/coap/oscore-support/oscore-context.c
+++ os/net/app-layer/coap/oscore-support/oscore-context.c
@@ -115,7 +115,7 @@ bytes_equal(const uint8_t *a_ptr, uint8_t a_len, const uint8_t *b_ptr, uint8_t b
 }
 
 #ifdef WITH_GROUPCOM
-void
+bool
 oscore_derive_ctx(oscore_ctx_t *common_ctx,
   const uint8_t *master_secret, uint8_t master_secret_len,
   const uint8_t *master_salt, uint8_t master_salt_len,
@@ -123,10 +123,10 @@ oscore_derive_ctx(oscore_ctx_t *common_ctx,
   const uint8_t *sid, uint8_t sid_len,
   const uint8_t *rid, uint8_t rid_len,
   const uint8_t *id_context, uint8_t id_context_len,
-  uint8_t replay_window,
+  uint8_t replay_window_size,
   const uint8_t *gid)
 #else
-void
+bool
 oscore_derive_ctx(oscore_ctx_t *common_ctx,
   const uint8_t *master_secret, uint8_t master_secret_len,
   const uint8_t *master_salt, uint8_t master_salt_len,
@@ -134,7 +134,7 @@ oscore_derive_ctx(oscore_ctx_t *common_ctx,
   const uint8_t *sid, uint8_t sid_len,
   const uint8_t *rid, uint8_t rid_len,
   const uint8_t *id_context, uint8_t id_context_len,
-  uint8_t replay_window)
+  uint8_t replay_window_size)
 #endif
 {
   uint8_t info_buffer[15];
@@ -172,15 +172,22 @@ oscore_derive_ctx(oscore_ctx_t *common_ctx,
 
   common_ctx->recipient_context.recipient_id = rid;
   common_ctx->recipient_context.recipient_id_len = rid_len;
-  common_ctx->recipient_context.largest_seq = -1;
+  /*common_ctx->recipient_context.largest_seq = -1;
   common_ctx->recipient_context.recent_seq = 0;
   common_ctx->recipient_context.replay_window_size = replay_window;
   common_ctx->recipient_context.rollback_largest_seq = 0;
   common_ctx->recipient_context.sliding_window = 0;
   common_ctx->recipient_context.rollback_sliding_window = -1;
-  common_ctx->recipient_context.initialized = 0;
+  common_ctx->recipient_context.initialized = 0;*/
+
+  if (!oscore_sliding_window_init(&common_ctx->recipient_context.sliding_window, replay_window_size))
+  {
+    return false;
+  }
 
   list_add(common_context_list, common_ctx);
+
+  return true;
 }
 
 void
