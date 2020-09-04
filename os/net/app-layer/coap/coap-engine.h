757aceee01db6bec0a6f74daa6d93011831672ed
Martin Gunnarsson <martin.gunnarsson@ri.se>
2020-09-04 15:06:17 +0200

    minor changes for multicast

--- os/net/app-layer/coap/coap-engine.h
+++ os/net/app-layer/coap/coap-engine.h
@@ -49,9 +49,11 @@ typedef struct coap_periodic_resource_s coap_periodic_resource_t;
 
 #include "coap.h"
 #include "coap-timer.h"
-#ifdef WITH_GROUPCOM
+
+#ifdef WITH_OSCORE
 #include "oscore-crypto.h"
-#endif
+#endif /* WITH_OSCORE */
+
 typedef enum {
   COAP_HANDLER_STATUS_CONTINUE,
   COAP_HANDLER_STATUS_PROCESSED
@@ -78,7 +80,8 @@ void coap_remove_handler(coap_handler_t *handler);
 
 void coap_engine_init(void);
 
-#ifdef WITH_GROUPCOM
+//int coap_receive(const coap_endpoint_t *src,
+//                 uint8_t *payload, uint16_t payload_length, uint8_t is_mcast);
 
 //Code added for HW crypto support
 
@@ -86,13 +89,8 @@ coap_status_t coap_receive(uint8_t *payload, uint16_t payload_length, coap_messa
 int coap_receive_cont(const coap_endpoint_t *src,
              uint8_t *payload, uint16_t payload_length, uint8_t is_mcast, void *queue_entry, coap_status_t in_status, coap_message_t *msg, coap_message_t *resp);
 void coap_send_postcrypto(coap_message_t *message, coap_message_t *response);
-#else
 
-int coap_receive(const coap_endpoint_t *src,
-                 uint8_t *payload, uint16_t payload_length, uint8_t is_mcast);
-#endif
-
-//End of HW crypto code
+/*End of HW crypto code*/
 coap_handler_status_t coap_call_handlers(coap_message_t *request,
                                          coap_message_t *response,
                                          uint8_t *buffer,
