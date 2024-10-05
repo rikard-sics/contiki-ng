326a96035d7d9982d7bc94fa11634b580b1a5b6b
Rikard Höglund <rikard.hoglund@ri.se>
2024-10-05 17:32:31 +0200

    Use generate_info to create info structure rather than doing it inline

--- os/net/security/edhoc/edhoc.h
+++ os/net/security/edhoc/edhoc.h
@@ -391,9 +391,9 @@ uint8_t edhoc_get_authentication_key(edhoc_context_t *ctx);
  * Get the size of the resulting CBOR byte string which would be produced by wrapping
  * a byte array of len as a CBOR byte string.
  */
-int cbor_bstr_size(uint32_t len);
+uint8_t cbor_bstr_size(uint32_t len);
 
-
+uint8_t cbor_int_size(int16_t num);
 int edhoc_authenticate_msg(edhoc_context_t *ctx, uint8_t **ptr, uint8_t cipher_len, uint8_t *ad, cose_key_t *key);
 int8_t edhoc_check_rx_msg(uint8_t *buffer, uint8_t buff_sz);
 int8_t edhoc_check_rx_msg_2(uint8_t *buffer, uint8_t buff_sz,edhoc_context_t* ctx);
